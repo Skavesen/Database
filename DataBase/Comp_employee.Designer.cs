7f43ab96931534a18bc7007056070d26a8a4107a
Skavesen <92335820+Skavesen@users.noreply.github.com>
2021-10-11 23:34:05 +0300

    Add files via upload

--- DataBase/Comp_employee.Designer.cs
+++ DataBase/Comp_employee.Designer.cs
@@ -28,171 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.помощьДетскимСадамToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.помощьДетямToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button4 = new System.Windows.Forms.Button();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.menuStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 11F);
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.помощьДетскимСадамToolStripMenuItem,
-            this.помощьДетямToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(735, 28);
-            this.menuStrip1.TabIndex = 2;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // помощьДетскимСадамToolStripMenuItem
-            // 
-            this.помощьДетскимСадамToolStripMenuItem.Name = "помощьДетскимСадамToolStripMenuItem";
-            this.помощьДетскимСадамToolStripMenuItem.Size = new System.Drawing.Size(187, 24);
-            this.помощьДетскимСадамToolStripMenuItem.Text = "Помощь детским садам";
-            this.помощьДетскимСадамToolStripMenuItem.Click += new System.EventHandler(this.помощьДетскимСадамToolStripMenuItem_Click);
-            // 
-            // помощьДетямToolStripMenuItem
-            // 
-            this.помощьДетямToolStripMenuItem.Name = "помощьДетямToolStripMenuItem";
-            this.помощьДетямToolStripMenuItem.Size = new System.Drawing.Size(126, 24);
-            this.помощьДетямToolStripMenuItem.Text = "Помощь детям";
-            this.помощьДетямToolStripMenuItem.Click += new System.EventHandler(this.помощьДетямToolStripMenuItem_Click);
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 58);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(729, 468);
-            this.dataGridView1.TabIndex = 3;
-            this.dataGridView1.UserDeletingRow += new System.Windows.Forms.DataGridViewRowCancelEventHandler(this.dataGridView1_UserDeletingRow);
-            this.dataGridView1.DoubleClick += new System.EventHandler(this.dataGridView1_DoubleClick);
-            // 
-            // label1
-            // 
-            this.label1.BackColor = System.Drawing.SystemColors.Window;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(219, 28);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(256, 27);
-            this.label1.TabIndex = 5;
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // button4
-            // 
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button4.Location = new System.Drawing.Point(632, 1);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(97, 28);
-            this.button4.TabIndex = 13;
-            this.button4.Text = "Поиск";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // textBox3
-            // 
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox3.Location = new System.Drawing.Point(429, 3);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(197, 25);
-            this.textBox3.TabIndex = 15;
-            // 
-            // label5
-            // 
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label5.Location = new System.Drawing.Point(314, 538);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(415, 23);
-            this.label5.TabIndex = 17;
-            this.label5.TextAlign = System.Drawing.ContentAlignment.TopRight;
-            // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.Location = new System.Drawing.Point(11, 532);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(241, 26);
-            this.button2.TabIndex = 23;
-            this.button2.Text = "Добавить запись в таблицу";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button5
             // 
-            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.button5.Location = new System.Drawing.Point(318, 1);
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.button5.Location = new System.Drawing.Point(66, 46);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(96, 27);
+            this.button5.Size = new System.Drawing.Size(118, 39);
             this.button5.TabIndex = 29;
             this.button5.Text = "Генерация записей";
             this.button5.UseVisualStyleBackColor = true;
-            this.button5.Visible = false;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
-            // label2
+            // comboBox1
             // 
-            this.label2.BackColor = System.Drawing.SystemColors.Window;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(219, 28);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(256, 27);
-            this.label2.TabIndex = 30;
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "район",
+            "тип собственности",
+            "вид помощи садам",
+            "форма собственности",
+            "вид помощи детям",
+            "группа",
+            "детский сад",
+            "предприятие",
+            "сотрудник детского сада",
+            "сотрудник предприятия",
+            "ребёнок",
+            "помощь детским садам",
+            "помощь детям"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(230, 28);
+            this.comboBox1.TabIndex = 30;
             // 
             // Comp_employee
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(735, 570);
-            this.Controls.Add(this.label2);
+            this.ClientSize = new System.Drawing.Size(254, 95);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button5);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
-            this.MaximumSize = new System.Drawing.Size(751, 608);
-            this.MinimumSize = new System.Drawing.Size(751, 608);
             this.Name = "Comp_employee";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.DB_FormClosing);
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ToolStripMenuItem помощьДетскимСадамToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem помощьДетямToolStripMenuItem;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
